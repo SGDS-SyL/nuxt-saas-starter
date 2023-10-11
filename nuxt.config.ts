b4fe0221ff7c62a6ea9c75bcd9160d418ae67ad8
SyL <yongliang@infinitetrinity.com.sg>
2023-10-11 14:24:41 +0800

    Added TailwindCss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://rsms.me/inter/inter.css',
+        },
+      ],
+    },
+  },
   typescript: {
     typeCheck: true,
   },
-});
+})
